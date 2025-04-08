b514c72cfb3cca2f6cfde3000283a26d57f1be8e
senvn <Vasyl.Senkovskyy@gmail.com>
2025-04-08 19:40:09 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,15 +1,1 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import CreateInventory from "./CreateInventory";
-import DashboardInventory from "./DashboardInventory";
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<CreateInventory />} />
-      <Route path="/dashboard" element={<DashboardInventory />} />
-    </Routes>
-  );
-}
-
-export default App;
+// App.jsx placeholder
---
+++ DashboardInventory.jsx
@@ -0,0 +1,1 @@
+// DashboardInventory.jsx placeholder
